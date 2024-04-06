f2e5f52df47a117f1bfe2caec65a6407e18e8c7e
carnotbraun <carnotbraun@gmail.com>
2024-04-06 20:32:59 -0300

    feat:heat_edges_plot code for map vizualization

--- heat_edges_plot.py
+++ heat_edges_plot.py
@@ -1,13 +1,14 @@
 import pandas as pd
-import random
+import sumolib 
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.collections import LineCollection
 
+NET_FILE = '/Users/carnotbraun/tese-mestrado/simu/LuSTScenario/scenario/lust.net.xml'
 # Read data from co2_lust.txt
-df_edges = pd.read_csv('/Users/carnotbraun/tese-mestrado/code/co2_lust.txt', sep='\t', names=['edge', 'co2'])
+net = sumolib.net.readNet(NET_FILE)
+df_edges = pd.read_csv('/Users/carnotbraun/tese-mestrado/simu/data/heatmap/co2_lust.txt', sep='\t', names=['edge', 'co2'])
 df_edges['edge'] = df_edges['edge'].apply(lambda x: x.replace(' ', ''))
-
 # Calculate total CO2 emissions for each edge
 total_co2 = df_edges.groupby('edge')['co2'].sum()
 
@@ -20,13 +21,13 @@ for edge_id, co2 in total_co2.items():
     shapes.append(e.getShape())
     values.append(co2)
 
-# Normalize CO2 emissions for color mapping
-cNorm = matplotlib.colors.Normalize(vmin=min(values), vmax=max(values))
-scalarMap = matplotlib.cm.ScalarMappable(norm=cNorm, cmap='magma')
-colors = scalarMap.to_rgba(values)
+# Set color scale
+min_value = min(values)
+max_value = max(values)
+color_scale = matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=min_value, vmax=max_value), cmap='magma')
 
 # Create LineCollection for plotting
-line_segments = LineCollection(shapes, colors=colors, linewidths=[1,])
+line_segments = LineCollection(shapes, colors=color_scale.to_rgba(values), linewidths=[1,])
 
 # Plot the graph
 fig, axs = plt.subplots(figsize=(8, 8))
@@ -36,6 +37,6 @@ axs.xaxis.set_ticklabels([])
 axs.yaxis.set_ticklabels([])
 
 # Add colorbar
-plt.colorbar(scalarMap)
-
+plt.colorbar(color_scale, ax=axs)
+plt.title('LuST Edges CO2')
 plt.show()
