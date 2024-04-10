0ff2b7e177482ec6842d49d5a92c10fa38e0cc89
carnotbraun <carnotbraun@gmail.com>
2024-04-10 09:06:42 -0300

    feat:new features to the codes

--- heatmap_plot.py
+++ heatmap_plot.py
@@ -6,7 +6,10 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
- 
+
+# Set the font size of the plots
+plt.rcParams.update({'font.size': 14})
+
 def load_rsus_data(folder_path):
     """Load CO2 data from RSUs from CSV files in a folder."""
     dfs = []
@@ -16,6 +19,8 @@ def load_rsus_data(folder_path):
             rsu_id = extract_rsu_id(filename)
             try:
                 df = pd.read_csv(file_path, sep=',', header=0)
+                # Convert CO2 emission to kg
+                df['c02_emission'] /= 1000
                 # 15 minutes interval
                 df['step'] = pd.to_datetime(df['step'], unit='s')
                 df = df.groupby([pd.Grouper(key='step', freq='600s')]).sum().reset_index()
@@ -28,28 +33,27 @@ def load_rsus_data(folder_path):
 
 def extract_rsu_id(filename):
     """Extract the RSU ID from the file name."""
-    return filename.split('_')[-1].split('.')[0]
+    return str(filename.split('_')[-1].split('.')[0])
 
 def plot_heatmap(data, environment):
     """Plot a heatmap of CO2 emissions from RSUs."""
-    plt.figure(figsize=(6, 4))
+    plt.figure(figsize=(7, 5.5))
     ax = sns.heatmap(data, cmap='gnuplot_r')
+    #ax.set_xticks([0, 12, 23])
     ax.set_xlabel('Horário do Dia')
     ax.set_ylabel('RSU')
-    #plt.title(f'{environment} CO2 Emission')
     plt.savefig(f'{environment.lower()}_co2_heatmap', dpi=300, bbox_inches='tight')
     plt.show()
 
 def main():
     """Main function."""
     env = {1: 'Lust', 2: 'Most', 3: 'Cologne'}
-    env_id = 2  # Change to the desired environment
+    env_id = 3  # Change to the desired environment
     folder_path = f'/Users/carnotbraun/tese-mestrado/simu/data/rsus_{env[env_id].lower()}_csv'
     try:
         data = load_rsus_data(folder_path)
         data = data.set_index('rsu_id')
         data = data.reset_index()
-        print(data.head())
         pivot_df = data.pivot_table(index='rsu_id', columns='step', values='c02_emission', aggfunc='sum')
         plot_heatmap(pivot_df, env[env_id])
     except Exception as e:
--- timeseries_plot.py
+++ timeseries_plot.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+# Set the font size of the plots
+plt.rcParams.update({'font.size': 20})
+
 def load_data(env_id):
     """Load data from CSV files in a folder."""
     env = {1: 'Lust', 2: 'Most', 3: 'Cologne'}
@@ -22,7 +25,7 @@ def load_data(env_id):
         df['step'] = df['step'] / 3600  # Convert seconds to hours
         
         # Convert 'c02_emission' values from milligrams to grams
-        df['c02_emission'] = df['c02_emission'] / 1000  # Convert mg to g
+        df['c02_emission'] = df['c02_emission'] / 1000_000  # Convert mg to g
         
         dfs.append(df)
     
@@ -30,12 +33,14 @@ def load_data(env_id):
 
 def plot_co2_emission_over_time(data):
     """Plot CO2 emission over time."""
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(7, 5.5))
     data.groupby('step')['c02_emission'].sum().plot(kind='line', color='b')
+    plt.xlim(0, 24)
+    plt.xticks(range(0, 25, 4), [f'{i}:00' for i in range(0, 25, 4)])
     plt.xlabel('Horário do Dia')
-    plt.ylabel('Emissão de CO2 (g)')
-    plt.grid(True)
-    plt.savefig(f'co2_timeserie', dpi=300, bbox_inches='tight')
+    plt.ylabel('Emissão de CO2 (kg)')
+    plt.grid(True, linestyle=':', alpha=0.6)
+    plt.savefig(f'rsu_cologne', dpi=300, bbox_inches='tight')
     plt.show()
 
 def main():
